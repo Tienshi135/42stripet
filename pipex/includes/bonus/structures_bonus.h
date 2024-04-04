1dd0a725d6a6bd201d32ae1a567a21817200d917
Tienshi <sebspubemail@gmail.com>
2024-04-04 17:20:52 +0200

    first finished product

--- pipex/includes/bonus/structures_bonus.h
+++ pipex/includes/bonus/structures_bonus.h
@@ -6,7 +6,7 @@
 /*   By: tienshi <tienshi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 09:26:57 by stripet           #+#    #+#             */
-/*   Updated: 2024/04/03 17:24:50 by tienshi          ###   ########.fr       */
+/*   Updated: 2024/04/04 17:17:01 by tienshi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ typedef struct s_data{
 	int		input;
 	int		output;
 	char	*paths;
-	int		rval;
+	char	*path;
+	char	**args;
+	int		pid;
 }	t_data;
 
 #endif
--- pipex/source/bonus/utils_bonus.c
+++ pipex/source/bonus/utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tienshi <tienshi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:13:10 by stripet           #+#    #+#             */
-/*   Updated: 2024/04/04 16:48:58 by tienshi          ###   ########.fr       */
+/*   Updated: 2024/04/04 17:20:23 by tienshi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,62 +41,58 @@ static char	*findpath(t_data data, char *cmd)
 	return (NULL);
 }
 
+static void	get_path_and_args(t_data *data, char *cmd)
+{
+	data->args = ft_split(cmd, ' ');
+	data->path = findpath(*data, data->args[0]);
+	if (data->path == NULL)
+	{
+		if (data->args && *(data->args) != cmd)
+			free(data->args);
+		msg(*data, cmd_err);
+	}
+}
+
 void	child_proccess(t_data data, char *cmd)
 {
 	int		fd[2];
-	int		pid;
-	char	*path;
-	char	**args;
 
 	if (pipe(fd) == -1)
 		msg(data, pipe_err);
-	pid = fork();
-	if (pid == -1)
+	data.pid = fork();
+	if (data.pid == -1)
 		msg (data, fork_err);
-	if (pid == 0)
+	if (data.pid == 0)
 	{
-		args = ft_split(cmd, ' ');
-		path = findpath(data, args[0]);
-		if (!path)
-		{
-			if (args && *args != cmd)
-				free(args);
-			msg(data, cmd_err);
-		}
+		get_path_and_args(&data, cmd);
 		close (fd[0]);
 		if (dup2(fd[1], STDOUT_FILENO) == -1)
 			msg(data, dup_err);
-		execve(path, args, NULL);
+		execve(data.path, data.args, NULL);
 	}
 	else
 	{
 		close (fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		waitpid(pid, NULL, 0);
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+			msg(data, dup_err);
+		waitpid(data.pid, NULL, 0);
 	}
 }
 
 void	last_cmd(t_data data, char *cmd)
 {
-	int		pid;
-	char	*path;
-	char	**args;
-
-	pid = fork();
-	if (pid == -1)
+	data.pid = fork();
+	if (data.pid == -1)
 		msg(data, fork_err);
-	else if (pid != 0)
+	else if (data.pid == 0)
 	{
-		args = ft_split(cmd, ' ');
-		path = findpath(data, args[0]);
-		if (!path)
-			msg(data, cmd_err);
+		get_path_and_args(&data, cmd);
 		if (dup2(data.output, STDOUT_FILENO) == -1)
 			msg(data, dup_err);
-		execve(path, args, NULL);
+		execve(data.path, data.args, NULL);
 	}
 	else
 	{
-		waitpid(pid, NULL, 0);
+		waitpid(data.pid, NULL, 0);
 	}
 }
